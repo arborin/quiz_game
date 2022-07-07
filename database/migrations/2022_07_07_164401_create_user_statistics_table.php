556a137445f6ee3ce4f5313e347226520a42f0e6
arborin <nika.kobaidze@gmail.com>
2022-07-07 20:48:05 +0400

    [ADD] migration

---
+++ database/migrations/2022_07_07_164401_create_user_statistics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_statistics', function (Blueprint $table) {
+            $table->id();
+            $table->string('user')->notnull();
+            $table->integer('answered_question_count');
+            $table->integer('correct_question_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_statistics');
+    }
+};
